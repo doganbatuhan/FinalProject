93c48dc3afba79a6f0954f584bd15fff5c7761e0
doganbatuhan <batuuhan.dogan@gmail.com>
2021-03-13 23:53:23 +0300

    13 Mart Ders sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -44,6 +44,9 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
+
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -70,6 +73,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
